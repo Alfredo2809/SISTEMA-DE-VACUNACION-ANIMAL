464c1b8d2a196fb887561e77fa6ef4365a2ac667
Alfredo <alfredoalbertomartinezlopez03@gmail.com>
2026-08-19 15:22:58 -0600

    VAC-2-Alfredo López- repositorio usuario

--- src/main/java/org/esfe/vacunacion/repositorios/IUsuarioRepository.java
+++ src/main/java/org/esfe/vacunacion/repositorios/IUsuarioRepository.java
@@ -1,13 +1,26 @@
 package org.esfe.vacunacion.repositorios;
 
+import org.esfe.vacunacion.modelos.EstadoUsuario;
+import org.esfe.vacunacion.modelos.RolUsuario;
 import org.esfe.vacunacion.modelos.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
-    // Método para buscar por correo si lo necesitas para autenticación o validaciones
+    
     Optional<Usuario> findByCorreo(String correo);
+
+    boolean existsByCorreo(String correo);
+
+
+    List<Usuario> findByRol(RolUsuario rol);
+
+
+    List<Usuario> findByEstado(EstadoUsuario estado);
+
+    Optional<Usuario> findByCorreoAndEstado(String correo, EstadoUsuario estado);
 }
