2ddc86281fe6cc7325cce976bad8ec31714ac810
brian <darciabrian@gmail.com>
2026-08-24 20:18:14 -0600

    [VAC-57] Brian Enrique Darcia: Crear ViviendaService - Se implementa la interfaz e implementacion con inyeccion del repositorio

---
+++ src/main/java/org/esfe/vacunacion/servicios/ViviendaServiceImpl.java
@@ -0,0 +1,40 @@
+package org.esfe.vacunacion.servicios;
+
+import org.esfe.vacunacion.modelos.Vivienda;
+import org.esfe.vacunacion.repositorios.ViviendaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ViviendaServiceImpl implements IViviendaService {
+
+    @Autowired
+    private ViviendaRepository viviendaRepository;
+
+    @Override
+    public List<Vivienda> listarTodas() {
+        return viviendaRepository.findAll();
+    }
+
+    @Override
+    public Vivienda buscarPorId(Long id) {
+        return viviendaRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vivienda guardar(Vivienda vivienda) {
+        return viviendaRepository.save(vivienda);
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        viviendaRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Vivienda> listarPorColonia(Long idColonia) {
+        return viviendaRepository.listarPorColonia(idColonia);
+    }
+}
