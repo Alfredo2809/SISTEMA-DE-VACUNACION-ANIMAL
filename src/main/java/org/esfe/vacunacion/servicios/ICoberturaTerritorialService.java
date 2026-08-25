5a723d58028b14b51d6337c4658bbcf6a5657cab
brian <darciabrian@gmail.com>
2026-08-24 22:18:35 -0600

    [VAC-93] Brian Enrique Darcia: Crear CoberturaTerritorialService - Se implementa la interfaz e implementacion con inyeccion del repositorio

---
+++ src/main/java/org/esfe/vacunacion/servicios/ICoberturaTerritorialService.java
@@ -0,0 +1,18 @@
+package org.esfe.vacunacion.servicios;
+
+import org.esfe.vacunacion.modelos.CoberturaTerritorial;
+
+import java.util.List;
+
+public interface ICoberturaTerritorialService {
+
+    List<CoberturaTerritorial> listarTodas();
+
+    CoberturaTerritorial buscarPorId(Long id);
+
+    CoberturaTerritorial guardar(CoberturaTerritorial coberturaTerritorial);
+
+    void eliminar(Long id);
+
+    List<CoberturaTerritorial> listarPorCampana(Long idCampana);
+}
