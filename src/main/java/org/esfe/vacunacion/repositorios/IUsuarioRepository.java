c17458505d87f7b39a612c83c196cfdc7742955a
Alfredo <alfredoalbertomartinezlopez03@gmail.com>
2026-08-31 22:26:17 -0600

    usuario completo (modelos, repo, service, controller y vistas)

--- src/main/java/org/esfe/vacunacion/repositorios/IUsuarioRepository.java
+++ src/main/java/org/esfe/vacunacion/repositorios/IUsuarioRepository.java
@@ -3,6 +3,8 @@ package org.esfe.vacunacion.repositorios;
 import org.esfe.vacunacion.modelos.EstadoUsuario;
 import org.esfe.vacunacion.modelos.RolUsuario;
 import org.esfe.vacunacion.modelos.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,16 +13,17 @@ import java.util.Optional;
 
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
-    
+
     Optional<Usuario> findByCorreo(String correo);
 
     boolean existsByCorreo(String correo);
 
-
     List<Usuario> findByRol(RolUsuario rol);
-
 
     List<Usuario> findByEstado(EstadoUsuario estado);
 
     Optional<Usuario> findByCorreoAndEstado(String correo, EstadoUsuario estado);
+
+
+    Page<Usuario> findByNombreCompletoContainingIgnoreCase(String nombre, Pageable pageable);
 }
